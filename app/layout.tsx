5d922a656fedd225e70842c59f21499eb509db78
Anmol <146084770+AnmolGithubx@users.noreply.github.com>
2025-08-29 11:56:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'QR Code Generator',
+  description: 'Made with ❤️ and lots of coffee',
 }
 
 export default function RootLayout({
